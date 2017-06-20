e52d004c175004d6607e8e35644005153d8f336a
sayands1997 <sayandsarkar.1997@gmail.com>
2017-06-21 00:20:34 +0530

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,53 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {ToDo} = require('./../models/todo');
+
+beforeEach((done) => {
+   ToDo.remove({}).then(() => done()); 
+});
+
+describe('POST /todos', () => {
+   it('should create a new ToDo',(done) => {
+      var text = 'Test todo text';
+       
+       request(app)
+       .post('/todos')
+       .send({text})
+       .expect(200)
+       .expect((res) => {
+         expect(res.body.text).toBe(text);  
+       })
+       .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            ToDo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((e) => done(e));
+        });
+   }); 
+     
+    it('should not create todo with invalid body data', (done) => {
+        
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+           ToDo.find().then((todos) => {
+               expect(todos.length).toBe(0);
+               done();
+           }).catch((e) => done(e)); 
+        });
+        
+        
+    });
+    
+});
