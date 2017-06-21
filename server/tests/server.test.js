8d8b7d2d8031beaf64729a96ca85219aa91cac90
sayands1997 <sayandsarkar.1997@gmail.com>
2017-06-21 20:16:17 +0530

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,40 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+   it('should remove a todo', (done) => {
+       var hexID = todos[1]._id.toHexString();
+       request(app)
+       .delete(`/todos/${hexID}`)
+       .expect(200)
+       .expect((res) =>{
+           expect(res.body.todo._id).toBe(hexID);
+       })
+       .end((err, res)=>{
+          if(err){
+              return done(err);
+          }
+        ToDo.findById(hexID).then((todo) => {
+               expect(todo).toNotExist();
+                done();
+        }).catch((e) => done(e));
+     });
+  });
+    it('should return 404 if todo not found', (done) =>{
+         var hexID= new ObjectID().toHexString();
+        
+        request(app)
+        .delete(`/todos/${hexID}`)
+        .expect(404)
+        .end(done);
+    });
+    
+    it('should return 404 if object id is invalid',(done) => {
+        request(app)
+        .delete(`/todos/id/123abc`)
+        .expect(404)
+        .end(done);
+    });
+});
+
