22e2c86c22284c58043341dd31b0fd02e971e16b
sayands1997 <sayandsarkar.1997@gmail.com>
2017-06-22 14:07:46 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id : 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded:', decoded);
+//bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password,salt, (err,hash)=>{
+//      console.log(hash); 
+//   }); 
+//});
 
-//var message = 'I am user number 3';
+var hashedPassword = "$2a$10$nZLpiYHClAxFFK9Q2Y6kD.RnKGQBagH7FH4jgSuRCKyImRerkB1LK";
+
+bcrypt.compare(password,hashedPassword, (err,res)=>{
+    console.log(res);
+});
+
+//var data = {
+//    id : 10
+//};
+//
+//var token = jwt.sign(data,'123abc');
+//console.log(token);
+//var decoded = jwt.verify(token, '123abc');
+//console.log('Decoded:', decoded);
+//
+////var message = 'I am user number 3';
 //var hash = SHA256(message).toString();
 //
 //console.log(`Message: ${message}`);
--- server/server.js
+++ server/server.js
@@ -106,9 +106,6 @@ app.post('/users', (req,res) => {
      });
 });
 
-
-
-
 app.get('/users/me', authenticate, (req,res) => {
     res.send(req.user);
   });
